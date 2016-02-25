056fa64c9f1dcb62f8944657462da349c7a61ebd
Sheela Gowry Sumathi Raju <ssumathi@klass2-01-l.intra.itcollege.ee>
2016-02-25 16:06:33 +0200

    Add Preliminary Support for argument prasing

--- arturisthebest.py
+++ arturisthebest.py
@@ -1,24 +1,34 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Apache2 log parser.')
+parser.add_argument('--path', help='Path to Apache2 log files', default="/home/ssumathi/logs")
+parser.add_argument('--top-urls', help="Find top URL-s", action='store_true')
+parser.add_argument('--geoip', help ="Resolve IP-s to country codes", action='store_true')
+parser.add_argument('--verbosity', help="Increase verbosity", action="store_true")
+args = parser.parse_args()
+
+
 import os
 import urllib
 import gzip
 # Following is the directory with log files,
 # On Windows substitute it where you downloaded the files
-root = "/home/ssumathi/logs"
 
 keywords = "Windows", "Linux", "OS X", "Ubuntu", "Googlebot", "bingbot", "Android", "YandexBot", "facebookexternalhit"
 d = {} 
 urls = {}
 users = {}
 total = 0
-for filename in os.listdir(root):
+for filename in os.listdir(args.path):
 	if not filename.startswith("access.log"):
-		print "Skipping unknown file:", filename
+		#print "Skipping unknown file:", filename
 		continue
 	if filename.endswith(".gz"):
-		fh = gzip.open(os.path.join(root, filename))
+		fh = gzip.open(os.path.join(args.path, filename))
 	else:
-		fh = open(os.path.join(root, filename))    
-	print "Going to process:", filename
+		fh = open(os.path.join(args.path, filename))    
+	if args.verbosity:
+		print "Going to process:", filename
 	for line in fh:
 		total = total + 1
 		try:
