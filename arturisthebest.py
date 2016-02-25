7bcd40f393a5ff0098948d035fb5e21ce1cf3dbc
Sheela Gowry Sumathi Raju <ssumathi@klass2-01-l.intra.itcollege.ee>
2016-02-25 17:26:44 +0200

    Humanize transferred bytes

--- arturisthebest.py
+++ arturisthebest.py
@@ -1,73 +1,89 @@
 import argparse
-
-parser = argparse.ArgumentParser(description='Apache2 log parser.')
-parser.add_argument('--path', help='Path to Apache2 log files', default="/home/ssumathi/logs")
-parser.add_argument('--top-urls', help="Find top URL-s", action='store_true')
-parser.add_argument('--geoip', help ="Resolve IP-s to country codes", action='store_true')
-parser.add_argument('--verbosity', help="Increase verbosity", action="store_true")
-args = parser.parse_args()
-
-
 import os
 import urllib
-import gzip
-# Following is the directory with log files,
-# On Windows substitute it where you downloaded the files
+
+parser = argparse.ArgumentParser(description='Apache2 log parser.')
+parser.add_argument('--path',
+    help="Path to Apache2 log files", default="/home/ssumathi/logs")
+parser.add_argument('--top-urls',
+    help="Find top URL-s", action='store_true')
+parser.add_argument('--geoip',
+    help="Resolve IP-s to country codes", action='store_true') # We'll implement this later ;)
+parser.add_argument('--verbose',
+    help="Increase verbosity", action="store_true")
+args = parser.parse_args()
 
 keywords = "Windows", "Linux", "OS X", "Ubuntu", "Googlebot", "bingbot", "Android", "YandexBot", "facebookexternalhit"
-d = {} 
+d = {} # Curly braces define empty dictionary
 urls = {}
-users = {}
+user_bytes = {}
+
 total = 0
+import gzip
 for filename in os.listdir(args.path):
-	if not filename.startswith("access.log"):
-		#print "Skipping unknown file:", filename
-		continue
-	if filename.endswith(".gz"):
-		fh = gzip.open(os.path.join(args.path, filename))
-	else:
-		fh = open(os.path.join(args.path, filename))    
-	if args.verbosity:
-		print "Going to process:", filename
-	for line in fh:
-		total = total + 1
-		try:
-			source_timestamp, request, response, referrer, _, agent, _ = line.split("\"")
-			method, path, protocol = request.split(" ")
-			path = urllib.unquote(path)
-			if path.startswith("/~"):
-				username, remainder = path[2:].split("/",1)
-				#print "User found:", username
-				try:
-					users[username] = users[username] + 1
-				except:
-					users[username] = 1
+    if not filename.startswith("access.log"):
+        continue
+    if filename.endswith(".gz"):
+        fh = gzip.open(os.path.join(args.path, filename))
+    else:
+        fh = open(os.path.join(args.path, filename))
+    if args.verbose:
+        print "Parsing:", filename
+    for line in fh:
+        total = total + 1
+        try:
+            source_timestamp, request, response, referrer, _, agent, _ = line.split("\"")
+            method, path, protocol = request.split(" ")
+        except ValueError:
+            continue # Skip garbage
+            
+        if path == "*": continue # Skip asterisk for path
 
-			url = "http://enos.itcollege.ee" + path
-			try:
-				urls[url] = urls[url] + 1
-			except:
-				urls[url] = 1
+        _, status_code, content_length, _ = response.split(" ")
+        content_length = int(content_length)
+        path = urllib.unquote(path)
+        
+        if path.startswith("/~"):
+            username = path[2:].split("/")[0]
+            try:
+                user_bytes[username] = user_bytes[username] + content_length
+            except:
+                user_bytes[username] = content_length
 
-			for keyword in keywords:
-				if keyword in agent:
-					try:
-						d[keyword] = d[keyword] + 1
-					except KeyError:
-						d[keyword] = 1
-					break # Stop searching for other keywords
-		except ValueError:
-			pass # This will do nothing, needed due to syntax
- 
-print "----------TOP 5 USERS----------"
-print "Total lines:", total
-userresult = users.items()
-userresult.sort(key = lambda item:item[1], reverse=True)
-for keyword, hits in userresult[:5]:
-	print keyword, "=", hits, "(", hits * 100 / total, "%)"
-print "----------TOP 5 PAGES----------"
+        try:
+            urls[path] = urls[path] + 1
+        except:
+            urls[path] = 1
+        
+        for keyword in keywords:
+            if keyword in agent:
+                try:
+                    d[keyword] = d[keyword] + 1
+                except KeyError:
+                    d[keyword] = 1
+                break
+
+def humanize(bytes):
+    if bytes < 1024:
+        return "%d B" % bytes
+    elif bytes < 1024 ** 2:
+        return "%.1f kB" % (bytes / 1024.0)
+    elif bytes < 1024 ** 3:
+        return "%.1f MB" % (bytes / 1024.0 ** 2)
+    else:
+        return "%.1f GB" % (bytes / 1024.0 ** 3)
+
+    
+print
+print("Top 5 bandwidth hoggers:")
+results = user_bytes.items()
+results.sort(key = lambda item:item[1], reverse=True)
+for user, transferred_bytes in results[:5]:
+    print user, "==>", humanize(transferred_bytes)
+    
+print
+print("Top 5 visited URL-s:")
 results = urls.items()
 results.sort(key = lambda item:item[1], reverse=True)
-for keyword, hits in results[:5]:
-    print keyword, "==>", hits, "(", hits * 100 / total, "%)"
-print "-------------------------------"
+for path, hits in results[:5]:
+    print "http://enos.itcollege.ee" + path, "==>", hits, "(", hits * 100 / total, "%)"
